9d0490374b4b954f9fd5683f3cc95b54d44f2924
Thomas BTR <thomatito971@gmail.com>
2023-01-19 17:30:34 +0100

    finish course

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -2,15 +2,17 @@ import {Component, Input, OnInit} from '@angular/core';
 import * as url from "url";
 import {FaceSnap} from "../models/face-snap.models";
 import {FaceSnapsService} from "../services/face-snaps.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit{
+export class FaceSnapComponent implements OnInit {
 
-  constructor(private faceSnapService: FaceSnapsService) {
+  constructor(private faceSnapService: FaceSnapsService,
+              private router: Router) {
   }
 
 
@@ -24,12 +26,16 @@ export class FaceSnapComponent implements OnInit{
   }
 
   onSnap() {
-    if(this.buttonText == this.snapText) {
+    if (this.buttonText == this.snapText) {
       this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'snap');
       this.buttonText = this.unSnapText;
     } else {
       this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
       this.buttonText = this.snapText;
     }
+  }
+
+  onViewFaceSnap() {
+    return this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,29 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FaceSnapsService} from "../services/face-snaps.service";
 import {FaceSnap} from "../models/face-snap.models";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-single-face-snap',
   templateUrl: './single-face-snap.component.html',
   styleUrls: ['./single-face-snap.component.scss']
 })
-export class SingleFaceSnapComponent implements OnInit{
-
-  constructor(private faceSnapService: FaceSnapsService) {
-  }
+export class SingleFaceSnapComponent implements OnInit {
 
 
-  @Input() faceSnap!: FaceSnap;
+  faceSnap!: FaceSnap;
   buttonText!: string;
   snapText: string = 'Oh Snap!';
   unSnapText: string = 'Oops UnSnap!';
+  constructor(private faceSnapService: FaceSnapsService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.buttonText = this.snapText;
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapService.getFaceSnapById(faceSnapId);
   }
 
   onSnap() {
-    if(this.buttonText == this.snapText) {
+    if (this.buttonText == this.snapText) {
       this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'snap');
       this.buttonText = this.unSnapText;
     } else {
